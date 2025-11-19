b31e704a91cbf5fce92b95c7781bf2d032f18b8c
MustLuikH7 <mokamartin3@gmail.com>
2025-11-19 23:38:49 +0200

    töötab2

--- KooliProjekt.WebAPI/Program.cs
+++ KooliProjekt.WebAPI/Program.cs
@@ -1,11 +1,12 @@
-using FluentValidation;
 using KooliProjekt.Application.Behaviors;
 using KooliProjekt.Application.Data;
 using Microsoft.AspNetCore.Builder;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.EntityFrameworkCore;
+using FluentValidation;
+using MediatR;
 
 namespace KooliProjekt.WebAPI
 {
