2889bc9774875e1dd5342d8a428521d70446df0a
MustLuikH7 <mokamartin3@gmail.com>
2026-05-04 14:34:13 +0300

    BlazorWasm

---
+++ KooliProjekt.BlazorWasm/Api/IApiClient.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KooliProjekt.BlazorWasm
+{
+    public interface IApiClient
+    {
+        Task<OperationResult<PagedResult<Users>>> List(int page, int pageSize);
+        Task<OperationResult> Save(Users list);
+        Task<OperationResult> Delete(int id);
+    }
+}
--- KooliProjekt.WebAPI/Program.cs
+++ KooliProjekt.WebAPI/Program.cs
@@ -26,6 +26,18 @@ namespace KooliProjekt.WebAPI
             });
 
             builder.Services.AddControllers();
+            
+            // Add CORS policy
+            builder.Services.AddCors(options =>
+            {
+                options.AddPolicy("AllowAll", policy =>
+                {
+                    policy.AllowAnyOrigin()
+                          .AllowAnyMethod()
+                          .AllowAnyHeader();
+                });
+            });
+
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
@@ -48,6 +60,8 @@ namespace KooliProjekt.WebAPI
                 app.UseSwagger();
                 app.UseSwaggerUI();
             }
+
+            app.UseCors("AllowAll");
 
             app.UseAuthorization();
             app.MapControllers();
